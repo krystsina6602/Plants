779c4ce58d99df50358957afe0a054bc68e31a94
krystsina6602 <krystsina6602@gmail.com>
2023-01-21 17:20:05 +0100

    docs: modified index.js style.css

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log('Screen width 768px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
 console.log('Screen width 380px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
 console.log('No scrollbar for page widths from 1440px to 320px +15');
-console.log('Adaptive menu:\n ')
+console.log('Adaptive menu: burger icon at page width 380px +2,\n adaptine menu matches layout color scheme +4\n');
