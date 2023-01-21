d772096f34c641135d83bbb0f146d8679e9265f7
krystsina6602 <krystsina6602>
2023-01-21 22:48:42 +0100

    docs: modified adaptive.css index.html index.js style.css add file burger.js

---
+++ burger.js
@@ -0,0 +1,9 @@
+let burger = document.querySelector(".burger");
+let nav = document.querySelector(".navigation");
+
+burger.addEventListener("click", mobileMenu);
+
+function mobileMenu(header__navigation) {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+}
