c9797b188004c57022d42d5fd286f7fcd71e351a
krystsina6602 <krystsina6602@gmail.com>
2023-01-21 17:03:51 +0100

    docs: index.html index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log()
+console.log('Screen width 768px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
+console.log('Screen width 380px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
+console.log('No scrollbar for page widths from 1440px to 320px +15');
+console.log('Adaptive menu:\n ')
