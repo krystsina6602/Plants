7e37a524ba59b40092c35a71cfea3ceef43594c8
krystsina6602 <krystsina6602>
2023-01-22 23:04:21 +0100

    docs: modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 console.log('Screen width 768px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
 console.log('Screen width 380px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
 console.log('No scrollbar for page widths from 1440px to 320px +15');
