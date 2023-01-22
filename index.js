33bcbf1d690cac96e0441f7b8b5c264e5ca83e8c
krystsina6602 <krystsina6602>
2023-01-22 22:56:27 +0100

    docs: modified index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 console.log('Screen width 768px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
 console.log('Screen width 380px:\n blocks <header>, <footer>\n sections: welcome, about, service, prices, contacts +24');
 console.log('No scrollbar for page widths from 1440px to 320px +15');
-console.log('Adaptive menu: burger icon at page width 380px +2,\n adaptine menu matches layout color scheme +4\n');
+console.log('Adaptive menu: burger icon at page width 380px +2,\n adaptine menu matches layout color scheme +4,\n when you click on the burger icon, an adaptive menu smoothly appears +4,\n when you click on the cross, the adaptive menu is smoothly hidden leaving the screen +4,\n links on adaptive menu works providing smooth scrolling through anchors +4,\n when clicking on a link in the adaptive menu (or outside this window), the adaptive menu is smoothly hidden +4');
